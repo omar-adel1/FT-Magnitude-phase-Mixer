46dce43753279e0abb638eda030cb7bee2ce226c
omar-adel1 <108310359+omar-adel1@users.noreply.github.com>
2023-05-12 21:57:30 +0300

    DSP

--- Image_class.py
+++ Image_class.py
@@ -60,20 +60,30 @@ class Images:
     """  
       
     def get_component(self,component) :
-        self.image_read = self.image_read / np.max(self.image_read)
+       # self.image_read = self.image_read / np.max(self.image_read)
         
-        if component == "FT Magnitude" :
+        if component == "Magnitude" :
+            # magnitude_normalized = cv2.normalize(self.magnitude, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            # return magnitude_normalized
             return self.magnitude
         
-        elif component == "FT Phase" :
+        elif component == "Phase" :
+            # phase_normalized = cv2.normalize(self.phase, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            # return phase_normalized
             return self.phase
         
-        elif component == "FT Real component" :
+        elif component == "Real" :
+            # real_normalized = cv2.normalize(self.real, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            # return real_normalized 
             return self.real
         
-        elif component == "FT Imaginary component" :
-            return self.imaginary    
-     """   
+        elif component == "Imaginary" :
+            # imaginary_normalized = cv2.normalize(self.imaginary, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            # return imaginary_normalized    
+            return self.imaginary
+        
+        elif component == "Uniform magnitude" :
+            return self.uniform_magnitude
         
         elif component == "Uniform phase" :
             return self.uniform_phase
@@ -104,26 +114,61 @@ class Images:
              st.image(imaginary_normalized, clamp=True)
           #  st.image( self.imaginary , clamp=True )  
 
-
-
-
-"""        
-class ImageProcessor :
     
-    def __init__(self,image1,image2) :
-        self.image_1_component = None
-        self.image_2_component = None
-        self.Mixing_ratio = None
-        self.mixing_output_1 = None
-        self.mixing_output_2 = None
-        self.image_1 = None
-        self.image_2 = None
-            
-
-"""        
+    def inverse_fourier_image(image):
+        Inverse_fourier_image = np.real(np.fft.ifft2(image))  
         
-  
+        return Inverse_fourier_image
+    
+
+    @staticmethod
+    def Mix_Images(img_1 :'Images' ,img_2 :'Images', component_image_1 : str,component_image_2 :str,Mix_ratio_1: float,Mix_ratio_2:float) :
+        
+        #Get fourier parameters for each image 
+        Mag_img1 = img_1.get_component("Magnitude")
+        Phase_img1 = img_1.get_component("Phase")
+        Real_img1 = img_1.get_component("Real")
+        Imag_img1 = img_1.get_component("Imaginary")
+        UniPhase_img1 = img_1.get_component("Uniform phase")
+        UniMag_img1 = img_1.get_component("Uniform magnitude")
+        
+        Mag_img2 = img_2.get_component("Magnitude")
+        Phase_img2 = img_2.get_component("Phase")
+        Real_img2 = img_2.get_component("Real")
+        Imag_img2 = img_2.get_component("Imaginary")
+        UniPhase_img2 = img_2.get_component("Uniform phase")
+        UniMag_img2 = img_2.get_component("Uniform magnitude")
+        
+        Mix_ratio_1 = Mix_ratio_1/100
+        Mix_ratio_2 = Mix_ratio_2/100
+        print(Mix_ratio_1)
+        if component_image_1 == "Magnitude"  and component_image_2 == "Phase":
+            Mixed_Mag = Mag_img1*Mix_ratio_1 + Mag_img2*(1-Mix_ratio_1)
+            Mixed_Phase = Phase_img2*Mix_ratio_2 + Phase_img1*(1-Mix_ratio_2)
+            Mixed_FT = np.multiply(Mixed_Mag , np.exp(1j * Mixed_Phase ) )
+        elif component_image_1 == "Phase" and component_image_2 == "Magnitude" :
+            Mixed_Phase = Phase_img1*Mix_ratio_1 + Phase_img2*(1-Mix_ratio_1)
+            Mixed_Mag = Mag_img2*Mix_ratio_2 + Mag_img1*(1-Mix_ratio_2)
+            Mixed_FT = np.multiply(Mixed_Mag , np.exp(1j * Mixed_Phase) )
+        elif component_image_1 == "Real" and component_image_2 == "Imaginary"   :
+            New_real = Real_img1*Mix_ratio_1 + Real_img2*(1-Mix_ratio_1)
+            New_Imag = Imag_img2*Mix_ratio_2 + Imag_img1*(1-Mix_ratio_2)
+            Mixed_FT = New_real + 1j * New_Imag
             
+        elif component_image_1 == "Imaginary" and component_image_2 == "Real"  :
+            New_Imag = Imag_img1*Mix_ratio_1 + Imag_img2*(1-Mix_ratio_1)
+            New_real = Real_img2*Mix_ratio_2 + Real_img1*(1-Mix_ratio_2) 
+            Mixed_FT = New_real + 1j * New_Imag
+            
+        elif component_image_1 == "Magnitude" and component_image_2 == "Uniform phase" :
+            Mixed_Mag = Mag_img1*Mix_ratio_1 + Mag_img2*(1-Mix_ratio_1)
+            Mixed_Phase = UniPhase_img2*Mix_ratio_2 + Phase_img1*(1-Mix_ratio_2)   #Matrix of zeros 
+            Mixed_FT = np.multiply(Mixed_Mag , np.exp(1j * Mixed_Phase))
+            
+        elif component_image_1 == "Uniform phase" and component_image_2 == "Magnitude" :
+            Mixed_Phase = UniPhase_img1*Mix_ratio_1 + Phase_img2*(1-Mix_ratio_1)
+            Mixed_Mag = Mag_img2*Mix_ratio_2 + Mag_img1*(1-Mix_ratio_2)
+            Mixed_FT = np.multiply(Mixed_Mag,np.exp(1j* Mixed_Phase))
         
         elif component_image_1 == "Uniform magnitude"  and component_image_2 == "Phase":
             Mixed_Mag = UniMag_img1*Mix_ratio_1 + Mag_img2 * (1-Mix_ratio_1)   # Equivalent to UniMag_img1 or UniMag_img1
@@ -135,4 +180,22 @@ class ImageProcessor :
             Mixed_Mag = UniMag_img2*Mix_ratio_2 + Mag_img1*(1-Mix_ratio_2)
             Mixed_FT = np.multiply(Mixed_Mag,np.exp(1j*Mixed_Phase))
         
-        
+        elif component_image_1 == "Uniform magnitude" and component_image_2 == "Uniform phase":
+            Mixed_Mag = UniMag_img1*Mix_ratio_1 + Mag_img2* (1-Mix_ratio_1)
+            Mixed_Phase = UniPhase_img2*Mix_ratio_2 +Phase_img1*(1-Mix_ratio_2)
+            Mixed_FT = np.multiply(Mixed_Mag, np.exp(1j * Mixed_Phase))
+            
+        elif component_image_1 == "Uniform phase" and component_image_2 == "Uniform magnitude":
+            Mixed_Phase = UniPhase_img1*Mix_ratio_1 + Phase_img2*(1-Mix_ratio_1)
+            Mixed_Mag = UniMag_img2*Mix_ratio_2 + Mag_img1*(1-Mix_ratio_2)
+            Mixed_FT =  np.multiply(Mixed_Mag, np.exp(1j * Mixed_Phase))  
+            
+        else:
+           st.warning("Invalid Combination")
+           return
+        Image_combined = np.real(np.fft.ifft2(np.fft.ifftshift(Mixed_FT)))
+        Image_combined = cv2.normalize(Image_combined, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # Image_combined = np.real(np.fft.ifft2(Mixed_FT))
+        return Image_combined
+    
